803b4c3d71cbec1c80cf4333d74828aa9bae79e4
youtu@tencent.com <youtu@tencent.com>
2015-10-14 12:02:09 +0800

    fix ssl build bug.

--- thirdparty/curl/build.sh
+++ thirdparty/curl/build.sh
@@ -8,7 +8,7 @@ BASEDIR=`pwd`
 cd curl-7.40.0 
 
 echo "install to $BASEDIR"
-./configure --prefix="$BASEDIR" --disable-shared --enable-static --without-libidn --without-ssl --without-librtmp --without-gnutls --without-nss --without-libssh2 --without-zlib --without-winidn --disable-rtsp --disable-ldap --disable-ldaps --disable-ipv6 
+./configure --prefix="$BASEDIR" --disable-shared --enable-static --without-libidn --without-librtmp --without-gnutls --without-nss --without-libssh2 --without-zlib --without-winidn --disable-rtsp --disable-ldap --disable-ldaps --disable-ipv6 
 make -j5
 make install
 echo "make ok clean"
